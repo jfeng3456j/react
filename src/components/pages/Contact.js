3700066f993695e4b14070c6442d2909faaea48c
jfeng3456j <jfeng3456j@gmail.com>
2021-11-03 15:02:54 -0500

    add contact.js

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React, {Fragment}from 'react';
+
+const Contact = () => {
+    return (
+        <Fragment>
+            <h1>Contact Us</h1>
+            <p>11900 spring leaf dr, USA</p>
+            <p>Cell: 1-888-888-9000</p>
+        </Fragment>
+    )
+}
+
+export default Contact;
